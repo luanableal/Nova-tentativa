88b2302d16c21461cdedee66573193e8bdca29e2
luanableal <luanabls@gmail.com>
2021-12-03 14:47:41 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let button = document.querySelector("button")
+let conteudo = document.querySelector(".conteudo")
+
+button.addEventListener("click", (event) =>{
+
+    event.preventDefault();
+
+    fetch('https://jsonplaceholder.typicode.com/todos/')
+    .then(response => response.json())
+    .then(json => {
+
+        conteudo.innerHTML =  json.map( todos => `<div class="todos"> <h2>${todos.title}</h2> <p>${todos.id}</p></div>`).join("");
+
+        
+
+       
+    })
+})
+
